bd29930997091a2312140a6392c54d0ae066ab93
Wasaanyi <jwasaanyi@gmail.com>
2023-07-22 13:52:51 +0300

    updating the submit button

--- src/components/submitButton.js
+++ src/components/submitButton.js
@@ -4,7 +4,7 @@ import {ArrowDownIcon} from "@heroicons/react/20/solid";
 export const SubmitButton = () => (
     <div className="flex items-center justify-center mt-3">
         <hr className="w-full h-px bg-blue-500 my-4"/>
-        <button type="submit" className="flex items-center justify-center bg-blue-500 text-white px-4 py-4 rounded-full hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+        <button id="submit" title="submit button" type="submit" className="flex items-center justify-center bg-blue-500 text-white px-4 py-4 rounded-full hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
             <ArrowDownIcon className="w-5 h-5 " />
         </button>
         <hr className="w-full h-px bg-blue-500 my-4 md:hidden"/>
